561bdf4e37e744acb8bf9ec348b931322cc9f8ab
Dilara Uluturhan <dilarauluturhan@outlook.com>
2024-02-02 00:41:55 +0300

    feat: homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,22 @@
+import styles from "@/app/ui/homepage.module.css";
+import Link from "next/link";
+import { LogIn, Zap } from "lucide-react";
+
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <Zap size={50} color="#ffffff" strokeWidth={1.3} />
+          <span>Admin Dashboard</span>
+        </div>
+        <Link href="/login" type="submit" className={styles.button}>
+          <span>Login Page</span>
+          <LogIn />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
